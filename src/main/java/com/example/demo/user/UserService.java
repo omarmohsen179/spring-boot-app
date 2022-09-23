bed2c5139888b1cd2d29688a0ab4c69e2eb96fb0
omarmohsen179 <mohsenomar350@gmail.com>
2022-09-23 16:48:55 +0200

    omar

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,20 +1,16 @@
 package com.example.demo.user;
 
+import com.example.demo.configuration.exception.Types.RequestException;
 import com.example.demo.configuration.security.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,7 +28,20 @@ public class UserService  implements UserDetailsService {
         return userRepository.findById(Id);
     }
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-    public User Save(User user){
+    public User Add(User user){
+        if(userRepository.findByUsername(user.getUsername()).isPresent()){
+            throw new RequestException("username taken");
+        }
+        if(userRepository.findByUsername(user.getEmail()).isPresent()){
+            throw new RequestException("email taken");
+        }
+        return userRepository.save(user);
+    }
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
+    public User Update(User user){
+        if(userRepository.findById(user.getId()).isEmpty()){
+            throw new RequestException("user not found");
+        }
         return userRepository.save(user);
     }
 
